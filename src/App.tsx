b0f6bc3b89f7884e78012cdf10cc7bd900ef4843
nclsgg <nicolasguadagno@gmail.com>
2021-08-21 17:00:08 -0300

    Modal componentization

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import Modal from 'react-modal';
 import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement('#root');
 
@@ -23,13 +24,7 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/> 
       <Dashboard />
-
-      <Modal
-                 isOpen={isNewTransactionModalOpen}
-                 onRequestClose={handleCloseNewTransactionModal}
-                >
-                    <h2>Cadastrar transação</h2>
-                </Modal>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
       <GlobalStyle />    
     </>
   );
