7aa3c9f5faaae9b88ffa6c6793689205d432d3c0
nclsgg <nicolasguadagno@gmail.com>
2021-08-21 18:52:49 -0300

    Context Transactions scturcture

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { GlobalStyle } from "./styles/global";
 import Modal from 'react-modal';
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import { TransactionsContext } from "./TransactionsContext";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -22,12 +22,12 @@ export function App() {
     }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/> 
       <Dashboard />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
       <GlobalStyle />    
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
 
